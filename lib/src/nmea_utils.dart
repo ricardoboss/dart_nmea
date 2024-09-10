3412a0c8a41c01f888f50fc917fc63d28275ed9f
Ricardo Boss <mail@ricardoboss.de>
2024-09-10 23:31:51 +0200

    Prevent instantiation of utility class

--- lib/src/nmea_utils.dart
+++ lib/src/nmea_utils.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 /// A utility class for nmea.
 class NmeaUtils {
+  /// Utility classes should not be instantiated.
+  NmeaUtils._();
+
   /// Calculates the XOR checksum for the given input. The result consists of two
   /// hexadecimal, uppercase characters (00 - FF). The default encoding is US
   /// ASCII, but you can provide your own [encoding] if you wish.
