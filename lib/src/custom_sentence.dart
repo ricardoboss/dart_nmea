e7ed007ec6eef0983c7b7b9969d270c12c569889
Ricardo Boss <mail@ricardoboss.de>
2024-09-10 23:34:04 +0200

    Fix wrong doc comment

--- lib/src/custom_sentence.dart
+++ lib/src/custom_sentence.dart
@@ -4,7 +4,7 @@ import 'package:nmea/nmea.dart';
 /// long as it fits in the general envelope of an NMEA0183 sentence (begins with
 /// '$' and ends with windows-style newline characters).
 class CustomSentence extends NmeaSentence {
-  /// The [CustomChecksumSentence] constructor forwards all arguments to the
+  /// The [CustomSentence] constructor forwards all arguments to the
   /// [NmeaSentence] constructor, except the [type] parameter.
   /// The [type] is always [NmeaSentenceType.custom].
   CustomSentence({required this.identifier, required super.raw, super.prefix})
